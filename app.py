599cfb7b302690c1942ea1a0f450e9632d2f3a9e
Jakub <u160_jaklip_waw@technischools.com>
2025-12-31 20:31:23 +0100

    add user ID generation logic

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 users = {}
+
+def get_first_free_id():
+    if not users:
+        return 1
+    existing_ids = sorted(users.keys())
+    for i, uid in enumerate(existing_ids, 1):
+        if uid != i:
+            return i 
+    return existing_ids[-1] +12
